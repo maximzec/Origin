03bf66099e0d8974442c9269abbe9a872a254d25
Max Frolenko <maximzec3200@gmail.com>
2017-11-13 19:38:04 +0300

    Added plus and minus functions

--- app/src/main/java/com/samsung/myitschool/origin/MainActivity.java
+++ app/src/main/java/com/samsung/myitschool/origin/MainActivity.java
@@ -2,6 +2,9 @@ package com.samsung.myitschool.origin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void plus(View v){
+        EditText num1 = (EditText)this.findViewById(R.id.num1);
+        String snum1 = num1.getText().toString();
+        EditText num2 = (EditText)this.findViewById(R.id.num2);
+        String snum2 = num2.getText().toString();
+        TextView out = (TextView)this.findViewById(R.id.out);
+        out.setText(""+(Double.parseDouble(snum1) + Double.parseDouble(snum2)));
+
+
+    }
+    public void minus(View v){
+        EditText num1 = (EditText)this.findViewById(R.id.num1);
+        String snum1 = num1.getText().toString();
+        EditText num2 = (EditText)this.findViewById(R.id.num2);
+        String snum2 = num2.getText().toString();
+        TextView out = (TextView)this.findViewById(R.id.out);
+        out.setText(""+(Double.parseDouble(snum1) - Double.parseDouble(snum2)));
+
+    }
+
 }
